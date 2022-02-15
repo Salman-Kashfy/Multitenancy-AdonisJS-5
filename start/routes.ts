ba2811f87b674d59f46253d2ea3b531eb0c85897
Salman-Kashfy <salman.k@tekrevol.com>
2022-02-15 14:20:39 +0500

    Merge develop into  feature-auth

--- start/routes.ts
+++ start/routes.ts
@@ -24,7 +24,7 @@ Route.group(() => {
 
     /*
     |--------------------------------------------------------------------------
-    | Guest API Routes
+    | Guest Api Routes
     |--------------------------------------------------------------------------
     */
     Route.group(() => {
@@ -34,28 +34,28 @@ Route.group(() => {
         })
 
         // Login | Register | Verify email
-        Route.post('signup-parent', 'API/AuthController.signupParent')
-        Route.post('resend-signup-otp', 'API/AuthController.resendSignupOtp')
-        Route.post('verify-email', 'API/AuthController.verifyEmail')
-        Route.post('login', 'API/AuthController.login')
+        Route.post('signup-parent', 'Api/AuthController.signupParent')
+        Route.post('resend-signup-otp', 'Api/AuthController.resendSignupOtp')
+        Route.post('verify-email', 'Api/AuthController.verifyEmail')
+        Route.post('login', 'Api/AuthController.login')
         Route.post('social-login', 'Api/AuthController.socialLogin')
 
         // Reset Password
-        Route.post('forgot-password', 'API/AuthController.forgotPassword')
-        Route.post('verify-otp', 'API/AuthController.verifyOtp')
-        Route.post('reset-password', 'API/AuthController.resetPassword')
+        Route.post('forgot-password', 'Api/AuthController.forgotPassword')
+        Route.post('verify-otp', 'Api/AuthController.verifyOtp')
+        Route.post('reset-password', 'Api/AuthController.resetPassword')
     }).middleware('guest')
 
-    Route.post('logout', 'API/AuthController.logout')
+    Route.post('logout', 'Api/AuthController.logout')
 
     /*
     |--------------------------------------------------------------------------
-    | Authenticated API Routes
+    | Authenticated Api Routes
     |--------------------------------------------------------------------------
     */
     Route.group(() => {
-        Route.get('profile', 'API/AuthController.profile')
+        Route.get('profile', 'Api/AuthController.profile')
 
     }).middleware('auth')
 
-}).prefix('/api')
+}).prefix('/Api')
