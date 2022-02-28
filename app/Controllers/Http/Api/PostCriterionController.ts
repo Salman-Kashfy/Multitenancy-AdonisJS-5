71e48d6fa058b6a30420a78bc16e4efcc1b0de6c
Salman-Kashfy <salman.k@tekrevol.com>
2022-02-28 18:12:25 +0500

    Completed Post Criteria API

--- app/Controllers/Http/Api/PostCriterionController.ts
+++ app/Controllers/Http/Api/PostCriterionController.ts
@@ -1,9 +1,8 @@
 import ApiBaseController from 'App/Controllers/Http/Api/ApiBaseController'
 import {HttpContextContract} from '@ioc:Adonis/Core/HttpContext'
 import PostCriterionRepo from "App/Repos/PostCriterionRepo";
-import PostCriterionValidator from "App/Validators/PostCriterionValidator";
+import AddPostCriterionValidator from "App/Validators/AddPostCriterionValidator";
 import Attachment from "App/Models/Attachment";
-
 
 export default class PostCriterionController extends ApiBaseController {
 
@@ -12,14 +11,17 @@ export default class PostCriterionController extends ApiBaseController {
     }
 
     async store(ctx: HttpContextContract, instanceType?: number, mediaType?: String) {
-        await ctx.request.validate(PostCriterionValidator)
+        await ctx.request.validate(AddPostCriterionValidator)
         let input = ctx.request.only(this.repo.fillables())
+        await this.repo.restrictIfExist(input.subscription_id,input.role_id)
         let row = await PostCriterionRepo.store(input, ctx.request, instanceType || Attachment.TYPE[this.repo.model.name.toUpperCase()], mediaType)
         return this.apiResponse('Record Added Successfully', row)
     }
 
     async update(ctx: HttpContextContract, instanceType?: number, mediaType?: String): Promise<{ data: any; message: string; status: boolean }> {
-        await ctx.request.validate(PostCriterionValidator)
+        await ctx.request.validate(AddPostCriterionValidator)
+        let input = ctx.request.only(this.repo.fillables())
+        await this.repo.restrictIfExist(input.subscription_id,input.role_id,ctx.request.param('id'))
         return super.update(ctx, instanceType, mediaType)
     }
 
--- app/Repos/PostCriterionRepo.ts
+++ app/Repos/PostCriterionRepo.ts
@@ -1,6 +1,6 @@
 import BaseRepo from 'App/Repos/BaseRepo'
 import PostCriterion from "App/Models/PostCriterion";
-
+import ExceptionWithCode from 'App/Exceptions/ExceptionWithCode'
 
 class PostCriterionRepo extends BaseRepo {
     model
@@ -10,6 +10,18 @@ class PostCriterionRepo extends BaseRepo {
         super(PostCriterion, relations)
         this.model = PostCriterion
     }
+
+    async restrictIfExist(subscriptionId,roleId,excludeId){
+        let query = this.model.query().select('id').where('role_id',roleId).where('subscription_id',subscriptionId)
+        if(excludeId){
+            query = query.where('id','!=',excludeId)
+        }
+        let postCriteria = await query.first()
+        if(postCriteria){
+            throw new ExceptionWithCode('A criteria for this role and subscription already exists.',200)
+        }
+    }
 }
+
 
 export default new PostCriterionRepo()
