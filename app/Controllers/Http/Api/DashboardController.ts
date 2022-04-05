fb42d25482cf01e8bc3c8ff254c8a2e14ef9d1e4
Salman-Kashfy <salman.k@tekrevol.com>
2022-04-05 10:54:22 +0500

    Fix dashboard pending report count

--- app/Controllers/Http/Api/DashboardController.ts
+++ app/Controllers/Http/Api/DashboardController.ts
@@ -2,18 +2,17 @@ import ApiBaseController from "App/Controllers/Http/Api/ApiBaseController";
 import CategoryRepo from "App/Repos/CategoryRepo";
 import UserRepo from "App/Repos/UserRepo";
 import ReportRepo from "App/Repos/ReportRepo";
-import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class DashboardController extends ApiBaseController{
 
     async dashboardData(){
         const category = await CategoryRepo.model.query().select('id').getCount('id as count').first()
         const users = await UserRepo.model.query().select('id').getCount('id as count').first()
-        const pendingReports = await ReportRepo.model.query().select('id',Database.raw('COUNT(DISTINCT instance_id) as count')).where('status',ReportRepo.model.STATUSES.PENDING).groupBy(['instance_id','instance_type']).first()
+        const pendingReports = await ReportRepo.model.query().select('id').where('status',ReportRepo.model.STATUSES.PENDING).groupBy(['instance_id','instance_type'])
         const data = {
             categoryCount:category.$extras.count,
             userCount:users.$extras.count,
-            pendingReportCount: pendingReports ? pendingReports.$extras.count : 0,
+            pendingReportCount: pendingReports ? pendingReports.length : 0,
         }
         return this.apiResponse('Record fetched Successfully !',data)
     }
