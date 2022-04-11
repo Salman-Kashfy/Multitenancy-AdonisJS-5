9af6493e04ea45a456d27b1c5fb17a546c9e5f86
Salman-Kashfy <salman.k@tekrevol.com>
2022-04-11 18:24:27 +0500

    Completed in-app purchases

--- app/Middleware/Premium.ts
+++ app/Middleware/Premium.ts
@@ -1,11 +1,12 @@
 import {HttpContextContract} from '@ioc:Adonis/Core/HttpContext'
 import ExceptionWithCode from "App/Exceptions/ExceptionWithCode";
+import UserSubscriptionRepo from 'App/Repos/UserSubscriptionRepo'
 
 export default class Premium {
     public async handle({auth}: HttpContextContract, next: () => Promise<void>) {
-        const {user}:any = auth
-        const subscription = await user.related('subscription').query().first()
-        if(!subscription.amount){
+        const {user} = auth
+        const userSubscription = await UserSubscriptionRepo.model.query().where('user_id', user?.id).first()
+        if(!userSubscription){
             throw new ExceptionWithCode("You need a premium subscription to manage a park", 403)
         }
         await next()
